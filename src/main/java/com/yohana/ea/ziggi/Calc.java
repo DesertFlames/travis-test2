14cf0d20392a5314b91f827c4b6d8db5869811ab
Yohana <yohana@yohanakhoury.com>
2014-01-03 01:09:14 +0200

    Initial commit

---
+++ src/main/java/com/yohana/ea/ziggi/Calc.java
@@ -0,0 +1,30 @@
+package com.yohana.ea.ziggi;
+
+/**
+ * @author Yohana
+ *         Date: 1/3/14
+ */
+public class Calc {
+    private int i;
+    private int j;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    public int calc() {
+        return i+j;
+    }
+}
---
+++ src/test/java/com/yohana/ea/ziggi/CalcTest.java
@@ -0,0 +1,19 @@
+package com.yohana.ea.ziggi;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author Yohana
+ *         Date: 1/3/14
+ */
+public class CalcTest {
+    @Test
+    public void testName() throws Exception {
+        Calc calc = new Calc();
+        calc.setI(1);
+        calc.setJ(2);
+        Assert.assertEquals(calc.calc(),3);
+
+    }
+}
